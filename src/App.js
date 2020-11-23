3299303e3217a735a50746959c8287ec3fea7f18
micajank <micajank@umich.edu>
2020-11-23 09:07:19 -0800

    Login with Authrnticator and AuthWrapper

--- src/App.js
+++ src/App.js
@@ -71,8 +71,43 @@ const App = () => {
         </Router>
     )
 }
-
 export default App;
+
+
+
+//--------------------Original withAuthorization --------------------//
+
+
+// import React, { useEffect, useState } from 'react'
+// import Amplify, { API, graphqlOperation } from 'aws-amplify'
+// import { BrowserRouter as Router } from 'react-router-dom'
+// import { withAuthenticator } from '@aws-amplify/ui-react'
+// import Content from './content/Content'
+// import { Auth } from 'aws-amplify';
+// // Stuff we don't have yet
+// // import jwtDecode from 'jwt-decode'
+// // import './scss/main.scss' 
+// const App = props => {
+//     // implement updateuser function and pass it to content
+//     Auth.currentSession()
+//         .then(data => console.log(data))
+//         .catch(err => console.log(err));
+//     const user = {
+//         name:"John Doe",
+//         email:"123@test.com"
+//     }
+//     return (
+//         <Router>
+//             <div className="App">
+//                 <Content user={Auth} />
+//             </div>
+//         </Router>
+//     )
+// }
+// export default withAuthenticator(App)
+
+//--------------------Original withAuthorization --------------------//
+
 // =======
 //   }
 
